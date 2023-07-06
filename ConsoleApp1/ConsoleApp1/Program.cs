02e0856d6b6b0438930cf2c14f2dacacc21f3b56
AsgarMadadli <asgarmm@code.edu.az>
2023-07-06 05:09:58 -0700

    task compleyted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Tarih girin (dd _ MM _ yyyy):");
+        string userInput = Console.ReadLine();
+
+        try
+        {
+            DateTime date = DateTime.ParseExact(userInput, "dd _ MM _ yyyy", null);
+            Console.WriteLine("Tarih: " + date.ToString());
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Hata: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Beklenmeyen bir hata oluştu: " + ex.Message);
+        }
+    }
+}
